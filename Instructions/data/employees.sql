6838675a2c557994a7010064571db89a9cbfd66f
jacquiemcb <53495414+jacquiemcb@users.noreply.github.com>
2019-10-24 00:05:33 -0400

    Add files via upload

---
+++ Instructions/data/employees.sql
@@ -0,0 +1,47 @@
+departments
+-
+dept_no  VARCHAR
+dept_name VARCHAR
+
+
+dept_emp
+-
+emp_no INT
+dept_no VARCHAR
+from_date DATE
+to_date  DATE
+
+
+dept_manager
+-
+dept_no VARCHAR
+emp_no INT
+from_date DATE
+to_date DATE
+
+
+employees
+-
+emp_no INT 
+birth_date DATE
+first_name VARCHAR
+last_name  VARCHAR
+gender   VARCHAR
+hire_date DATE
+
+
+salaries
+-
+emp_no  INT
+salary INT
+from_date DATE
+to_date DATE
+
+
+titles
+-
+emp_no  INT 
+title VARCHAR
+from_date DATE
+to_date DATE
+
